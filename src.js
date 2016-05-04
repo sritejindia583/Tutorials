045b62fb1ecaf00c45ebf1eb34ae5c089ade48f7
sritejindia583 <sritejindia@outlook.com>
2016-05-04 20:58:56 +0530

    Adding level 1 code

---
+++ src.js
@@ -0,0 +1,13 @@
+(function() {
+	var app = angular.module('store',[]);
+
+	app.controller('StoreController',function() {
+		this.product = gem;
+	});
+
+	var gem = {
+		name: 'Decahedron',
+		price: 2.95,
+		description: 'this is original description.this is original descriptionthis is original descriptionthis is original descriptionthis is original description'
+	}
+})();
